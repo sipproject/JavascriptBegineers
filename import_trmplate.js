103b181fbef0604186da5b4ed14a677cdd63795f
jhonash740 <72943327+jhonash740@users.noreply.github.com>
2021-09-30 23:58:12 +0530

    Added condition when the timeout exceeds

--- import_trmplate.js
+++ import_trmplate.js
@@ -5,4 +5,8 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The date and time is currently: " + dt.myDateTime());
   res.end();
+  res.start();
+  
+  if(res) 
+    console.log('start');
 }).listen(8080);
